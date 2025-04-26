76396b58d0563eaec0062b78f75df4a2f1b7d3ab
teguhpermadi <tguh.permadi@gmail.com>
2025-04-26 22:36:37 +0700

    Implement student model, migration, factory, and seeder with ULID support

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Student extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentFactory> */
+    use HasFactory, HasUlids, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'nisn',
+        'nis',
+        'photo',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            StudentSeeder::class,
         ]);
     }
 }
