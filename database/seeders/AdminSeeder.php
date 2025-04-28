166eb5c74706b303e05e313c11b46f4ea476dd48
teguhpermadi <tguh.permadi@gmail.com>
2025-04-28 17:08:12 +0700

    feat(seeder): add AdminSeeder and update StudentSeeder to create 500 students

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,17 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\Student;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class StudentSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Student::factory()->count(500)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
