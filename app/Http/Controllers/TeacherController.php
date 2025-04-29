873c1c31d4e2f01f8ed62fd650aafae32f11ca57
teguhpermadi <tguh.permadi@gmail.com>
2025-04-29 17:31:37 +0700

    feat(TeacherController, StoreTeacherRequest, UpdateTeacherRequest): implement Teacher resource with CRUD operations and validation rules

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teacher;
+use App\Http\Requests\StoreTeacherRequest;
+use App\Http\Requests\UpdateTeacherRequest;
+use Inertia\Inertia;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Teachers/Index', [
+            'teachers' => Teacher::latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Teachers/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTeacherRequest $request)
+    {
+        Teacher::create($request->validated());
+
+        return to_route('teachers.index')->with('success', 'Teacher created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Teacher $teacher)
+    {
+        return Inertia::render('Teachers/Show', [
+            'teacher' => $teacher,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Teacher $teacher)
+    {
+        return Inertia::render('Teachers/Edit', [
+            'teacher' => $teacher,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTeacherRequest $request, Teacher $teacher)
+    {
+        $teacher->update($request->validated());
+
+        return to_route('teachers.index')->with('success', 'Teacher updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($teachers)
+    {
+        // Check if $teachers is an array
+        if(is_array($teachers)) {
+            Teacher::whereIn('id', $teachers)->delete();
+        } else {
+            Teacher::destroy([$teachers]);
+        }
+
+        return to_route('teachers.index');
+    }
+}
